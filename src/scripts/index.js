c5889f9af393fe56a28984e00cd6a5df2222058a
KINGBOTs1 <ds767365@gmail.com>
2023-05-19 02:50:02 +0800

    Initial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,5 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.scss';
+import '../styles/responsive.scss';
+import './script.js';
+// import '@fortawesome/fontawesome-free/js/all';
